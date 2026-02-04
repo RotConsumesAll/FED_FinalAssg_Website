81cefedd76742c9ccda399d3783ee8f5d86c3ca3
jarelland-shouwa <songjarell@gmail.com>
2026-02-04 15:33:14 +0800

    Update x svg im filter chip creation function

--- scripts/operator/centres-filter.js
+++ scripts/operator/centres-filter.js
@@ -21,11 +21,7 @@ const applyButton = document.querySelector("#apply-button");
 function createFilterChip(filterPreText, filterText) {
   return `<button class="filter-row__item">
       <span>${filterPreText}<span class="filter-row__item__filter-value">${filterText}</span></span>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="filter-row__item__close"
-          viewBox="0 0 16 16">
-          <path
-              d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-      </svg>
+      <img src="../assets/icons/x.svg" alt="Filter icon" height="16" width="16" class="filter-row__item__close">
   </button>`;
 }
 
