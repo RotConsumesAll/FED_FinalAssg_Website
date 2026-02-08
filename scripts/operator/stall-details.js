ba0ee7a2aed6f4883640e7bd6dfb4bb0f55f6ddd
jarelland-shouwa <songjarell@gmail.com>
2026-02-08 14:58:49 +0800

    Add user checking to stall detail page

--- scripts/operator/stall-details.js
+++ scripts/operator/stall-details.js
@@ -3,13 +3,21 @@ import {
   assignStallSelectHandlers,
   activateEventForSelectedStallItem,
 } from "./stall-details-display.js";
+import { checkUserAndGetUid } from "../services/user-authentication-service.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const centreId = urlParams.get("centreId");
   const stallId = urlParams.get("stallId");
 
-  await renderSidebar(centreId);
-  assignStallSelectHandlers(centreId);
-  activateEventForSelectedStallItem(stallId);
+  try {
+    const uid = await checkUserAndGetUid("operator");
+
+    await renderSidebar(centreId);
+    assignStallSelectHandlers(centreId);
+    activateEventForSelectedStallItem(stallId);
+  } catch (error) {
+    window.location.href = "../../index.html";
+    return;
+  }
 });
