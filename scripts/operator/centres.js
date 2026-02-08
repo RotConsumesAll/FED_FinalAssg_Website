34a411cb32666634b35b56cb71ec677373540d1b
jarelland-shouwa <songjarell@gmail.com>
2026-02-08 11:48:13 +0800

    Add function to check whether user is signed out or unauthorised to view a page

--- scripts/operator/centres.js
+++ scripts/operator/centres.js
@@ -3,27 +3,25 @@ import {
   activateEventForSelectedCentreItem,
 } from "./centres-display.js";
 import { assignEventHandlers } from "./centres-assign-handlers.js";
-import { auth, onAuthStateChanged } from "../firebase/authentication.js";
+import { checkUserAndGetUid } from "../services/user-authentication-service.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const centreId = urlParams.get("centreId");
 
-  onAuthStateChanged(auth, async (user) => {
-    if (user) {
-      const uid = user.uid;
-      await renderSidebar(uid);
-      await assignEventHandlers();
-      sessionStorage.clear();
+  try {
+    const uid = await checkUserAndGetUid("operator");
+    await renderSidebar(uid);
+    await assignEventHandlers();
+    sessionStorage.clear();
 
-      if (centreId) {
-        activateEventForSelectedCentreItem(centreId);
-      } else {
-        document.querySelector("ul.sidebar__menu").firstElementChild.click();
-      }
+    if (centreId) {
+      activateEventForSelectedCentreItem(centreId);
     } else {
-      alert("You are signed out. Redirecting to HawkPortal login page.");
-      window.location.href = "../../index.html";
+      document.querySelector("ul.sidebar__menu").firstElementChild.click();
     }
-  });
+  } catch (error) {
+    window.location.href = "../../index.html";
+    return;
+  }
 });
